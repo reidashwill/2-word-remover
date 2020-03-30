93c42c0323f9da0354f642fb8b6dd1ea01a66471
NeverSettels <settels.eric@gmail.com>
2020-03-30 13:55:48 -0700

    add backend logic from repl it

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,12 @@ var split3 = function (input) {
 //frontend
 $(document).ready(function () {
 
+  $('#formOne').submit(function (event) {
+    event.preventDefault();
+    var input = $('#myString').val();
+    var output = split3(input);
+    $('#stringOutput').text(output)
+  })
+
+
 });
